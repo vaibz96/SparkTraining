a8e4125c6634ee2136c0cb1f5f64c1977dc6b9af
vaibz96 <vshanbha@stevens.edu>
2024-03-12 01:06:07 -0400

    Connecting to Mogodb external db read function

---
+++ mongo-read.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import Row
+from pyspark.sql import functions
+
+if __name__ == "__main__":
+    # Create a SparkSession
+
+    spark = SparkSession. \
+    builder.appName("MongoDBIntegration"). \
+    getOrCreate()
+
+    # Read it back from MongoDB into a new Dataframe
+
+    readUsers = spark.read \
+    .format("com.mongodb.spark.sql.DefaultSource")\
+    .option("uri", "mongodb://127.0.0.1/moviesdata.users")\
+    .load()
+
+    readUsers.createOrReplaceTempView("users")
+
+    readUsers.printSchema()
+
+    sqlDF = spark.sql("""
+    SELECT occupation, count(user_id) as cnt_usr
+    FROM users
+    GROUP BY occupation
+    ORDER BY cnt_usr DESC
+    """)
+
+    sqlDF.show()
